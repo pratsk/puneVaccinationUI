1d8f78ee3df0794e2d5d4d430a9c952188c07967
pratsk <pratikkolapkar61@gmail.com>
2021-05-01 20:06:54 +0530

    Search on pincode and date

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-$.ajax({ 
-  type: "GET",
-  dataType: "json",
-  url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date=01-05-2021",
-  success: function(data){  
-    var centerList = data.centers;
-    var content = "<table class='container sortable desktop'><thead><tr><th><h1>Vaccination Center</h1></th>"
+function createCenterListFromJson(centerList){
+  var content = "<table class='container sortable desktop'><thead><tr><th><h1>Vaccination Center</h1></th>"
     for(i=0; i<7; i++){
         var d = new Date();
         d.setDate(d.getDate() + i);
@@ -59,7 +54,148 @@ $.ajax({
       content += "</section>"
 
       $('#centerList').append(content);
+}
+
+function createCenterListForSearchResult(centerList){
+  if(centerList.length == 0){
+    alert("No data available")
+  }
+  var content = "<table class='container sortable desktop'><thead><tr><th><h1>Vaccination Center</h1></th>" + 
+                '<th><h1>Date & Time</h1></th>'+
+                '<th><h1>Availability</h1></th>'+
+                '<th><h1>Vaccine</h1></th>'+
+                '<th><h1>Slots</h1></th>' ;
+      content += " </thead><tbody>";
+
+      for(i=0; i<centerList.length; i++){
+        content += "<tr><td><div>"+
+            "<p class='my-1'>"+ centerList[i].name +"</p>"+
+            "<p class='my-1'> <span>"+ centerList[i].block_name +"-<span>"+
+            "<span>"+ centerList[i].pincode +"</span></p>" +
+            "<p class='my-1'>"+ centerList[i].fee_type +"</p>" +
+        "</div></td>" +
+          "<td><div>"+
+            "<p class='my-1'>"+ centerList[i].date +"</p>"+
+            "<p class='my-1'>"+ centerList[i].from + "-" +  centerList[i].to +"</p>"+
+          "</div></td>"+
+          
+          "<td><div>"+
+            "<p class='my-1'>"+ centerList[i].available_capacity +"</p>"+
+          "</div></td>"+
+
+          "<td><div>"+
+            "<p class='my-1'>"+ centerList[i].vaccine +"</p>"+
+          "</div></td><td>";
+
+        var slots = centerList[i].slots;
+        for(j=0; j<slots.length; j++){
+          content += "<p class='my-1'>"+slots[j] +"</p>";
+        }
+        content += "</td></tr>"
+      }
+      content += " </tbody></table>";
+
+      content += "<section id='table-carousel' class='owl-carousel'>"
+      for(i=0; i<centerList.length; i++){
+        content += "<table class='mobile container sortable'<thead class='tac'><th colspan='2'>" +
+            "<div>" +
+              "<p class='my-1'>"+ centerList[i].name +"</p>"+
+              "<p class='my-1'> <span>"+ centerList[i].block_name +"-<span>"+
+              "<span>"+ centerList[i].pincode +"</span></p>" +
+              "<p class='my-1'>"+ centerList[i].fee_type +"</p>" +
+            "</div></th></thead>" +
+
+            "<tbody>" +
+            "<tr>"+
+              "<td width='50%'>Date & Time</td>"+
+              "<td width='50%'><div>"+
+                "<p class='my-1'>"+ centerList[i].date +"</p>"+
+                "<p class='my-1'>"+ centerList[i].from + "-" +  centerList[i].to +"</p>"+
+              "</div></td>"+
+            "</tr>"+
+
+            "<tr>"+
+              "<td width='50%'>Vaccine</td>"+
+              "<td width='50%'><div>"+
+                "<p class='my-1'>"+ centerList[i].vaccine +"</p>"+
+              "</div></td>"+
+            "</tr>" +
+
+            "<tr>"+
+              "<td width='50%'>Availability</td>"+
+              "<td width='50%'><div>"+
+                "<p class='my-1'>"+ centerList[i].available_capacity +"</p>"+
+              "</div></td>"+
+            "</tr>" +
+
+            "<tr>"+
+              "<td width='50%'>Slots</td><td width='50%'>"
+
+
+            var slots = centerList[i].slots;
+              for(j=0; j<slots.length; j++){
+                content += "<p class='my-1'>"+slots[j] +"</p>";
+              }
+              content += "</td></tr></tbody>" +
+        "</table>"
+      }
+
+      content += "</section>"
+
+      $('#centerList').html("");
+      $('#centerList').append(content);
+}
+
+$(document).ready(function () {
+  var date = new Date().getDate() +"-"+ (new Date().getMonth()+1) +"-"+new Date().getFullYear();
+  if($("#date").val()!=""){
+    date = $("#date").val().substr(8) + "-" +  $("#date").val().substr(5,2) + "-" + $("#date").val().substr(0,4);
+  }
+  $.ajax({ 
+    type: "GET",
+    dataType: "json",
+    url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date="+date,
+    success: function(data){  
+      var centerList = data.centers;
+      createCenterListFromJson(centerList);
+      }
+  });
+
+  //called when key is pressed in textbox
+  $("#pincode").keypress(function (e) {
+    //if the letter is not digit then display error and don't type anything
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      return false;
+   }
+  });
+
+  $("#pincode").keyup(function (e) {
+    if(this.value.length == 6){
+      $("#searchButton").prop("disabled", false);
+    }else{
+      $("#searchButton").prop("disabled", true);
     }
+  });
+
+  $(document).on("click","#searchButton",function() {
+    if($("#date").val()!=""){
+      date = $("#date").val().substr(8) + "-" +  $("#date").val().substr(5,2) + "-" + $("#date").val().substr(0,4);
+    }
+    $.ajax({ 
+      type: "GET",
+      dataType: "json",
+      url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+$("#pincode")[0].value+"&date="+date,
+      success: function(data){
+        var centerList = data.sessions;
+        createCenterListForSearchResult(centerList);
+        }
+    });
+  });
+
+  $(document).on("click","#clearButton",function() {
+    location.reload();
+  });
+
 });
 
 
